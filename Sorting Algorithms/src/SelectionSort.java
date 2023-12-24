8bc04de26216ae5341b41f711f782eabe6751dfc
Anirudha5997 <dudhasagare.a@northeastern.edu>
2023-12-23 20:40:57 -0500

    Sorting Algorithms

--- Sorting Algorithms/src/SelectionSort.java
+++ Sorting Algorithms/src/SelectionSort.java
@@ -1,2 +1,35 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4,5,1,2,3};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void selectionSort(int[] arr){
+        for (int i = 0; i< arr.length; i++){
+            int last = arr.length - i - 1;
+            int max = getMaxIndex(arr, 0, last);
+            swap(arr, max, last);
+
+        }
+    }
+
+     static int getMaxIndex(int[] arr, int start, int last) {
+        int max = start;
+        for(int i = start; i < last; i++){
+            if(arr[max] < arr[i]){
+                max = i;
+            }
+        } return max;
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
