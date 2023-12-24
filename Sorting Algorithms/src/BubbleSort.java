f7c4b0ee25d31d58cc2dfe6e1e22fc3a5a8373cc
Anirudha5997 <dudhasagare.a@northeastern.edu>
2023-12-23 20:11:32 -0500

    Bubble Sort

--- Sorting Algorithms/src/BubbleSort.java
+++ Sorting Algorithms/src/BubbleSort.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        System.out.println("Sorting Algos");
+        int[] arr = {3,2,5,1,4};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int[] arr){
+        boolean isSwapped = false;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 1; j < arr.length - i; j++){
+                //swap if the item is smaller than the previous item
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    isSwapped = true;
+                }
+            }
+            if(!isSwapped){
+                break;
+            }
+        }
     }
 }
